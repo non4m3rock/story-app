6b3785d28a0f21e62538dd1a4e7f97c0d79aebc8
noname_rock <noname_rock@MacBook-Air-noname-rock.local>
2025-03-13 15:39:07 +0800

    submission

--- src/js/home.js
+++ src/js/home.js
@@ -4,8 +4,41 @@ const Home = {
   },
 
   async _initialData() {
-    const fetchRecords = await fetch('/src/public/data/DATA.json');
+    const fetchRecords = await fetch('/data/DATA.json');
     const responseRecords = await fetchRecords.json();
+    this._userStories = responseRecords.listStory; // Sesuaikan dengan struktur data
+    this._populateStoriesToCard(this._userStories);
+  },
+
+  _populateStoriesToCard(stories = null) {
+    if (!(typeof stories === 'object')) {
+      throw new Error(`Parameter stories should be an object.`);
+    }
+
+    if (!Array.isArray(stories)) {
+      throw new Error('Parameter stories should be an array.');
+    }
+
+    let totalStories = stories.length;
+
+    const storyCardsContainer = document.getElementById('storyCards');
+    storyCardsContainer.innerHTML = ''; // Clear existing cards
+
+    stories.forEach((story) => {
+      const card = document.createElement('div');
+      card.className = 'col-sm row-sm d-flex py-4';
+      card.innerHTML = `
+        <div class="card flex-fill" style="width: 18rem">
+          <img src="${story.photoUrl}" class="card-img-top" alt="${story.name}" />
+          <div class="card-body">
+            <h5 class="card-title">${story.name}</h5>
+            <p class="card-text">${story.description}</p>
+            <a href="#" class="btn btn-warning">Lihat Detail</a>
+          </div>
+        </div>
+      `;
+      storyCardsContainer.appendChild(card);
+    });
   },
 };
 
